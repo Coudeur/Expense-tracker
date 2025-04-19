5908ff13f8d6841663bcd6ac5b712b0bf420d5db
Coudeur <60295807+Coudeur@users.noreply.github.com>
2025-04-19 18:54:17 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+// Nom du cache
+const CACHE_NAME = 'expense-tracker-v1';
+
+// Fichiers à mettre en cache
+const urlsToCache = [
+  './',
+  './index.html'
+];
+
+// Installation du service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Récupération des ressources
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
